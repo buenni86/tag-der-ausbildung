ebda379941a8c6d8ca49476273a7aa9aa7f2fdca
Leon Prinz <leonprinz2002@googlemail.com>
2023-03-06 11:08:27 +0100

    teamDescriptions now show team informations when entering a team name in ?name=

---
+++ src/teamDescriptions/main.ts
@@ -0,0 +1,34 @@
+import { Root, Description } from "./teamDeclaration";
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const teamName = urlParams.get("name");
+
+async function getDescriptions() {
+    const descriptions: Root = await fetch("./descriptions.json").then((r) => r.json()).then((d) => {return d});
+    return descriptions.descriptions.filter(function(item){return item.name === teamName})[0];
+}
+
+function buildHTMLElement(json: Description){
+    const div = document.createElement("div");
+    div.classList.add("description");
+
+    const h1 = document.createElement("h1");
+    h1.innerHTML = json.fullName;
+
+    const location = document.createElement("p");
+    location.classList.add("location")
+    location.innerHTML = "Standort(e): " + json.location;
+
+    const p = document.createElement("p");
+    p.innerHTML = json.text;
+
+    div.appendChild(h1);
+    div.appendChild(location);
+    div.appendChild(p);
+    return div;
+}
+
+document.body.appendChild(buildHTMLElement(await getDescriptions()));
+
+export {}
---
+++ src/teamDescriptions/teamDeclaration.ts
@@ -0,0 +1,12 @@
+export interface Root {
+    descriptions: Description[]
+}
+
+export interface Description {
+    name: string
+    fullName: string
+    mailto: string
+    location: string
+    text: string
+}
+  
