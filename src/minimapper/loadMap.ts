39e53cd0c605f8a2f0769f883038b8fef8774a85
Leon Prinz <leonprinz2002@googlemail.com>
2023-03-21 14:58:38 +0100

    fix missing WA init in minimapper

--- src/minimapper/loadMap.ts
+++ src/minimapper/loadMap.ts
@@ -1,3 +1,18 @@
-console.log(WA.room.id);
+/// <reference types="@workadventure/iframe-api-typings" />
+
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+    console.log(WA.room.id);
+    
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
 
 export {}
