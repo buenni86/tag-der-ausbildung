9ed97db55fdb82811082a2d905766548d38abfe0
RaimeXLP <41763410+RaimeXLP@users.noreply.github.com>
2023-03-22 10:22:11 +0000

    Deploying to gh-pages from master @ b65ab237dbdbd3410b00be2b87bf87cc69393bfd 🚀

---
+++ assets/minimapper-37ee8eaa.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-3cfb730f.js";import{b as r}from"./init-22028896.js";WA.onInit().then(()=>{let o=WA.room.mapURL.substring(0,WA.room.mapURL.lastIndexOf("."))+".png",t=document.getElementById("mapImage");t.src=o,r().then(()=>{console.log("Scripting API Extra ready")}).catch(e=>console.error(e))}).catch(o=>console.error(o));
