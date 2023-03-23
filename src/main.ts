620cff62e4da2ac9b3c03a64fa21a0e13e34c011
Leon Prinz <leonprinz2002@googlemail.com>
2023-03-23 15:56:40 +0100

    remove popup callback

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,5 @@ wae.init().then(() => {
     wae.popUpNoArea("infoPopUp","Willkommen bei Workadveutre!\nBewege dich mit WASD, Pfeiltasten oder Rechtsklick.\nDieses Event wird teilweise Aufgezeichnet",[wae.buttons.close]);
     wae.minimapButton();
     wae.cameraEvent(640,735,"bühne");
-    wae.popUp("evsPopup","dbPlanetArea","Hier zu unserer DB-Planet Seite:",[{label: "OK",callback: (popup => {WA.nav.openTab("https://www.db.de/workadventure")})}])
+    wae.popUp("evsPopup","dbPlanetArea","Hier zu unserer DB-Planet Seite:",[{label: "OK",callback: (() => {WA.nav.openTab("https://www.db.de/workadventure")})}])
 });
